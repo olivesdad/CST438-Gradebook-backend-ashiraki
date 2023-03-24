f968b64d51fac0b13663708ff44fe9e0bf38230e
andy <ashiraki@csumb.edu>
2023-03-24 00:06:54 -0700

    assigment rabbit

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cst438.domain.Course;
@@ -13,6 +14,9 @@ import com.cst438.domain.CourseRepository;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.http.HttpTimeoutException;
 
 
 public class RegistrationServiceMQ extends RegistrationService {
@@ -43,17 +47,32 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@RabbitListener(queues = "gradebook-queue")
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
-		
-		//TODO  complete this method in homework 4
-		
+		System.out.println("Receive enrollment :" + enrollmentDTO);
+		Enrollment e = new Enrollment();
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+		if (c == null){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Course ID");
+		}
+		e.setCourse(c);
+		e.setStudentEmail(enrollmentDTO.studentEmail);
+		e.setStudentName(enrollmentDTO.studentName);
+		enrollmentRepository.save(e);
 	}
 
 	// sender of messages to Registration Service
 	@Override
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
-		 
-		//TODO  complete this method in homework 4
-		
+		System.out.println("Sending final grades " + course_id +" " + courseDTO);
+		rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
+		System.out.println("After sending final grades");
 	}
+
+	/*
+	@Override
+	public void sendFinalGrades(int course_id , CourseDTOG courseDTO) {
+		System.out.println("Sending final grades " + course_id +" " + courseDTO);
+		restTemplate.put(registration_url+"/course/"+course_id, courseDTO);
+		System.out.println("After sending final grades");
+	}*/
 
 }
